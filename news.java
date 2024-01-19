27bc671a76da7e8e74d265b27ab86fa3f4ae5b2c
Sahi Kolukuluri <sahi.kolukuluri99@gmail.com>
2024-01-19 00:48:25 -0600

    first program

---
+++ news.java
@@ -0,0 +1,22 @@
+class news{
+
+public static void  main(String a[])
+    {
+       int i =0;
+      int j=0;
+       while(i<=3)
+       {
+       
+       while(j<=4)
+       {
+        System.out.println("how " + i);
+        System.out.println("ru " + j);
+        j++;
+        i++;
+       }
+      
+       }
+       System.out.println("bye " + i);
+
+       }
+    }
